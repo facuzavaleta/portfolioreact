e8e58859fa79a96d63a8ff3886734bf41d542e40
facuzavaleta <facuzavaleta890@gmail.com>
2023-11-13 19:20:40 -0300

    terminado div projects

---
+++ src/components/Projects.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ProjectItem from './ProjectItem'
+import easybankImg from '../assets/easybank.jpg'
+import odinglandingpageImg from '../assets/odinlanding.jpg'
+import thisprojectImg from '../assets/thisproject.jpg'
+
+const Projects = () => {
+  return (
+    <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+        <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>
+        <p className='text-center py-8 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas possimus laudantium, pariatur aspernatur at praesentium laboriosam dolores dicta repellendus facilis exercitationem suscipit velit ducimus ad sequi iste eaque? Accusantium, quos!
+        </p>
+        <div className='grid sm:grid-cols-2 gap-12'>
+            <ProjectItem img={easybankImg} title='easybank' />
+            <ProjectItem img={odinglandingpageImg} title='odinlandingpage'/>
+            <ProjectItem img={thisprojectImg} title='thisproject'/>
+            <ProjectItem img={easybankImg} title='easybank'/>
+        </div>
+    </div>
+  )
+}
+
+export default Projects
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -23,7 +23,7 @@ const Sidenav = () => {
                 <GrProjects size={20}/>
                 <span className='pl-4'>Works</span>
               </a>
-              <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+              <a href="#projects" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                 <AiOutlineProject size={20}/>
                 <span className='pl-4'>Projects</span>
               </a>
@@ -50,7 +50,7 @@ const Sidenav = () => {
             <a href="#work" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
               <GrProjects size={20}/>
             </a>
-            <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+            <a href="#projects" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
               <AiOutlineProject size={20}/>
             </a>
             <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
